6c41e6293fb69f8a8a842d78976d303e98163979
Brett Challice <98037014+CChally@users.noreply.github.com>
2023-04-01 19:45:06 -0400

    Update main.cs

--- PointQuadTree/main.cs
+++ PointQuadTree/main.cs
@@ -12,27 +12,53 @@ namespace Driver
     {
         static void Main()
         {
-            PointQuadTree PQ = new PointQuadTree(3);    // Init with specified dimension (1D, 2D, 3D ... ND)
+            PointQuadTree PQ = new PointQuadTree(2);    // Init with specified dimension (1D, 2D, 3D ... ND)
             Point p;
             Random rand = new Random();     // For generating random point values
 
-           
+
             // Create 10 random points
+
+            var delete = new Point(2);
+            delete.Set(0, -20);
+            delete.Set(1, 20);
+
+            PQ.Insert(delete);
 
             for (int i = 0; i < 10; i++)   
             {
-                p = new Point(3);           // Zero Point
+                p = new Point(2);           // Zero Point
 
                 for (int j = 0; j < PQ.dimension; j++)  // Assign values to zero point
-                {
                     p.Set(j, rand.Next(-50,50));   // Lower bound 0, upper bound of 100
-                }
-
+                
                 // Insert into PointQuad
                 PQ.Insert(p);   
             }
-          
+
+            p = new Point(2);
+            p.Set(0, -50);
+            p.Set(1, 0);
+        
+            var d = new Point(2);
+            d.Set(0, 50);
+            d.Set(1, 50);
+
+           
+            PQ.Insert(d); // farthest right
+            PQ.Insert(p); // farthest left
+
             PQ.PrintQuadTree();
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            PQ.Delete(delete);
+            PQ.Delete(p);
+            PQ.Delete(d);
+
+            PQ.PrintQuadTree();
+
             Console.ReadKey();
         }
     }
